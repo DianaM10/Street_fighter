c8cc83aa37e35c8758891c71957f3bb736d60336
Bertie Croll <bert.croll@gmail.com>
2017-01-19 22:01:18 +0000

    Deck card updated to es6, shufflecards method created

--- client/src/top_trumps/deck.js
+++ client/src/top_trumps/deck.js
@@ -1,14 +1,16 @@
-var _ = require('underscore');
-//run npm install to install the underscore or lodash library
-var Deck = function(cards){
-     this.cards = cards;
+class Deck {
+  constructor(card) {
+    this.cards = cards
+  }
+
+  shuffleCards() {
+    for(let i = this.cards.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1))
+      let tempCard = this.cards[i]
+      this.cards[i] = this.cards[rand]
+      this.cards[rand] = tempCard
+    }
+  }
 }
 
-Deck.prototype = {
-
-     shuffleCards: function(){
-          this.cards = _.shuffle(this.cards);
-     }
-}
-
-module.exports = Deck;
+export default Deck
